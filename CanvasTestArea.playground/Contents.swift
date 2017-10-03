6916021e502a70a5c5bef09b376ecb1ec281da11
Sayles Keegan <ksayles@lcs-mba3139.lcs.on.ca>
2017-10-03 15:23:39 -0400

    Made progress on rainbow picture

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -20,17 +20,27 @@
 import Cocoa
 import PlaygroundSupport
 
+
+
 // Create a new canvas
-let canvas = Canvas(width: 400, height: 300)
+let canvas = Canvas(width: 500, height: 500)
+canvas.drawShapesWithBorders = false
+// Color in the blue rectangle
+canvas.fillColor = Color.init(hue: 200, saturation: 50, brightness: 100, alpha: 100)
+canvas.drawRectangle(centreX: 0, centreY: 0, width: 1000, height: 1200)
 
-canvas.fillColor = Color.blue
-canvas.drawRectangle(centreX: 225, centreY: 150, width: 400, height: 350)
+// draw sun
+canvas.fillColor=Color.yellow
+canvas.drawEllipse(centreX: 0, centreY: 500, width: 200, height: 200)
 
+// Draw Cloud
+canvas.fillColor=Color.white
+canvas.drawEllipse(centreX: 450, centreY: 500, width: 150, height: 150, borderWidth: 0)
 
-// Show the result
+canvas.drawEllipse(centreX: 500, centreY: 450, width: 150, height: 150, borderWidth: 0)
+
+canvas.lineColor = Color.orange
+
+// This code is necassary to see the result in the Assistant Editor at right
 PlaygroundPage.current.liveView = canvas.imageView
 
-//Draw the rainbow
-canvas.borderColor = Color.purple
-canvas.defaultBorderWidth = 50
-canvas.drawEllipse(centreX: 200, centreY: 0, width: 100, height: 100)
