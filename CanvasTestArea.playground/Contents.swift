aa3208013937c59f4b60483bc8c965fabec3cb5d
Sayles Keegan <ksayles@lcs-mba3139.lcs.on.ca>
2017-10-03 14:48:38 -0400

    Getting started with a rainbow scene

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -21,20 +21,16 @@ import Cocoa
 import PlaygroundSupport
 
 // Create a new canvas
-let canvas = Canvas(width: 300, height: 500)
+let canvas = Canvas(width: 400, height: 300)
 
-// Draw a circle at the origin with radius of 50 pixels
-canvas.drawEllipse(centreX: 0, centreY: 0, width: 50, height: 50)
+canvas.fillColor = Color.blue
+canvas.drawRectangle(centreX: 225, centreY: 150, width: 400, height: 350)
 
-// Show where the origin is
-canvas.drawAxes()
 
-// Draw an ellipse in a different color at the centre of the canvas
-canvas.fillColor = Color.green
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 100)
-
-// Add a rectangle to the top of the screen
-canvas.drawRectangle(centreX: 150, centreY: 400, width: 100, height: 50)
-
-// This code is necessary to see the result in the Assistant Editor at right
+// Show the result
 PlaygroundPage.current.liveView = canvas.imageView
+
+//Draw the rainbow
+canvas.borderColor = Color.purple
+canvas.defaultBorderWidth = 50
+canvas.drawEllipse(centreX: 200, centreY: 0, width: 100, height: 100)
